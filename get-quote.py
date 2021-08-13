8df719e818a9a48eeef2f80227067d839da0dfa8
goacfowo <88197240+goacfowo@users.noreply.github.com>
2021-08-13 17:53:18 +0300

    Complete ideas for next steps

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,25 @@
 import random
 
-def primary():
-
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-
+def get():
+  file = open("quotes.txt", "r")
+  quotes = file.readlines()
+  file.close()
   first = 0
   last = len(quotes) - 1
   rnd = random.randint(first, last)
-  
-  print(quotes[rnd])
+  print(quotes[rnd], end="")
+
+def add():
+  file = open("quotes.txt", "a")
+  quote = str(input(">>> "))
+  file.write(quote)
+  file.close()
 
 if __name__== "__main__":
-  primary()
+  mode = str(input("Would you like to get or add a quote? (G/a) ").lower() or "g")
+  if mode == "g":
+    get()
+  elif mode == "a":
+    add()
+  else:
+    print("Error: undefined input.")
